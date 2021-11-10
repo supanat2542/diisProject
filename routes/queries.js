0f1743c5842882fb4f4310ddf170a63515e8cfa0
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-11-10 21:47:49 +0700

    -

--- routes/queries.js
+++ routes/queries.js
@@ -1,5 +1,6 @@
 const { request } = require('express');
 const moment = require('moment');
+const { Pool } = require('pg');
 const pool = new Pool({
     connectionString : process.env.DATABASE_URL = 'postgres://wgawxbdqhysitb:2237e7d2f53e1d3f1a6b6655db1710e5cb0c37f1a5760ba1d45ed4de27bc8d89@ec2-3-229-166-245.compute-1.amazonaws.com:5432/dfefjelb0iakj2',
     ssl: {
@@ -9,11 +10,8 @@ const pool = new Pool({
 
 const getVisitor = async(req, res) => {
     try {
-        const result = await pool.query(`SELECT visitor_id, first_name, last_name, tel, category,id_civiliz,contract,time_start,time_stop,visitor.tag_address , tag_id
-        FROM diis.visitor
-        INNER join diis.tag 
-        ON tag.tag_address = visitor.tag_address 
-        order by time_stop desc
+        const result = await pool.query(`SELECT id , name
+        FROM visitor
         `);
         output = {
             status: "success",
