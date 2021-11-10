2c9aac39c7f6a183e7bfa4ae452533b377aa3205
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-11-10 21:37:40 +0700

    add module export

--- routes/apis.js
+++ routes/apis.js
@@ -2,4 +2,6 @@ var express = require('express');
 var router = express.Router();
 const db = require('./queries');
 
-router.get('/visitor',db.getVisitor)
+router.get('/visitor',db.getVisitor);
+
+module.exports = router;
