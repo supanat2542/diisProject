633000830c01910e199352824fba19e44c45eab1
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-11-18 03:22:52 +0700

    -

--- routes/queries.js
+++ routes/queries.js
@@ -1,6 +1,6 @@
 const { request } = require('express');
+const Pool = require('pg').Pool;
 const moment = require('moment');
-const { Pool } = require('pg');
 const pool = new Pool({
     connectionString : process.env.DATABASE_URL = 'postgres://wgawxbdqhysitb:2237e7d2f53e1d3f1a6b6655db1710e5cb0c37f1a5760ba1d45ed4de27bc8d89@ec2-3-229-166-245.compute-1.amazonaws.com:5432/dfefjelb0iakj2',
     ssl: {
