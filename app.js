5a3935ba5fec2b0ec86b5fed590dba03c1ece391
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-11-18 03:16:36 +0700

    -

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/apis', apisRouter);
+app.use('/api', apisRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
