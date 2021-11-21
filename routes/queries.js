3e1727879342c517cdf9eaf1075a8edf138a22b5
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-11-21 19:25:43 +0700

    -

--- routes/queries.js
+++ routes/queries.js
@@ -132,6 +132,7 @@ const getLocation = async(req, res) => {
 
 const getVisitor = async(req, res) => {
     try {
+        console.warn(req)
         let visitor_id = "NULL";
         let visitor_select = ``;
             if (req.query.visitor_id != undefined) {
@@ -902,137 +903,7 @@ const getData2 = async(req, res) => {
     res.json(output);
 }
 
-///////////////////////////// CRUD API FROM EVENT TABLE ////////////////////////////////////////
 
-/******************************** GET ALL EVENT ***********************************/
-const getEvent = async(req, res) => {
-    try {
-        const result = await pool.query(`SELECT scannerid, deviceid, "timestamp", distance FROM "event"`);
-        output = {
-            status: "success",
-            result: result
-        }
-    } catch (error) {
-        output = {
-            status: "failed",
-            result: error
-        };
-    }
-
-    res.json(output)
-}
-
-/********************************* CREATE EVENT *************************************/
-const createEvent = async(req, res) => {
-    try {
-        const sql = `INSERT INTO "event" (scannerid, deviceid, "timestamp", distance) VALUES('${req.body.scannerid}', '${req.body.deviceid}', '${req.body.timestamp}', ${req.body.distance})`;
-        console.log(sql);
-        const result = await pool.query(sql);
-        output = {
-            status: "success",
-            result: result
-        }
-    } catch (error) {
-        output = {
-            status: "failed",
-            result: error
-        };
-    }
-    res.json(output);
-}
-
-/******************************* UPDATE EVENT BY ID *******************************/
-const updateEvent = async(req, res) => {
-    try {
-        const result = await pool.query(`UPDATE "event" SET scannerid='${req.body.scannerid}', deviceid='${req.body.deviceid}', "timestamp"='${req.body.timestamp}', distance=${req.body.distance} where id = '${req.params.id}'`);
-        output = {
-            status: "success",
-            result: result
-        };
-    } catch (error) {
-        output = {
-            status: "failed",
-            result: error
-        };
-    }
-    res.json(output);
-}
-
-/**************************** DELETE EVENT BY ID *******************************/
-const deleteEvent = async(req, res) => {
-    try {
-        const result = await pool.query(`DELETE FROM event WHERE id = '${req.params.id}'`);
-        output = {
-            status: "success",
-            result: result
-        };
-    } catch (error) {
-        output = {
-            status: "success",
-            result: error
-        };
-    }
-    res.json(output);
-}
-
-/**************************** CREATE ARRAY EVENT BY ID *******************************/
-const createArrayEvent = async(req, res) => {
-    try {
-        for (let id in req.body) {
-
-            let scanner_id = "NULL";
-            let device_address = "NULL";
-            let device_name = "NULL";
-            let device_appearance = "NULL";
-            let device_manufacturerdata = "NULL";
-            let device_serviceuuid = "NULL";
-            let device_txpower = "NULL";
-            let device_rssi = "NULL";
-
-            if (req.body[id].scanner_id != undefined) {
-                scanner_id = req.body[id].scanner_id;
-            }
-            if (req.body[id].device_address != undefined) {
-                device_address = req.body[id].device_address;
-            }
-            if (req.body[id].device_name != undefined) {
-                device_name = req.body[id].device_name;
-            }
-            if (req.body[id].device_appearance != undefined) {
-                device_appearance = req.body[id].device_appearance;
-            }
-            if (req.body[id].device_manufacturerdata != undefined) {
-                device_manufacturerdata = req.body[id].device_manufacturerdata;
-            }
-            if (req.body[id].device_serviceuuid != undefined) {
-                device_serviceuuid = req.body[id].device_serviceuuid;
-            }
-            if (req.body[id].device_txpower != undefined) {
-                device_txpower = req.body[id].device_txpower;
-            }
-            if (req.body[id].device_rssi != undefined) {
-                device_rssi = req.body[id].device_rssi;
-            }
-
-            // const time = new Date(Date.now()).toISOString();
-            const time = moment().locale('th').format();;
-            const sql = `INSERT INTO scanlog(scanner_id, device_address, device_name, device_appearance, device_manufacturerdata, device_serviceuuid, device_txpower, scan_timestamp, device_rssi)
-            VALUES('${scanner_id}', '${device_address}', '${device_name}', '${device_appearance}', '${device_manufacturerdata}', '${device_serviceuuid}', ${device_txpower}, '${time}', ${device_rssi})`;
-            await pool.query(sql);
-        }
-
-        output = {
-            status: "success",
-            result: req.body.lenght
-        }
-    } catch (error) {
-        output = {
-            status: "failed",
-            result: error
-        };
-    }
-    res.json(output);
-}
 
 
 
@@ -1070,10 +941,5 @@ module.exports = {
     getEdit,
     updateData,
     getEditItem,
-    updateDataItem,
-    getEvent,
-    createEvent,
-    updateEvent,
-    deleteEvent,
-    createArrayEvent,
+    updateDataItem
 }
