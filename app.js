169adf7a1ea55a6197661cb9012b40082b24806e
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-11-21 12:51:21 +0700

    -

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ app.use(function(err, req, res, next) {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true); 
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  next(); 
   
 });
 
