4f83746be78a2c618b47f0d7365dfe0247a06857
Rusfirdao <Fiirdaoo@gmail.com>
2021-11-22 23:03:02 +0700

    -

--- routes/apis.js
+++ routes/apis.js
@@ -4,7 +4,7 @@ const db = require('./queries');
 
 
 router.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
     next()
