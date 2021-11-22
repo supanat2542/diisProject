8c91705028189550753f6bb37b4d2375b04baa7c
Rusfirdao <Fiirdaoo@gmail.com>
2021-11-22 23:00:31 +0700

    -

--- routes/apis.js
+++ routes/apis.js
@@ -3,7 +3,12 @@ var router = express.Router();
 const db = require('./queries');
 
 
-
+router.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:8081')
+    res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
+    next()
+  })
 
 
   router.get('/tags', db.getTag);
